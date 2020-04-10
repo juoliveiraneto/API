22579689c1a07e805e8b4bdac573909674944a2f
Juarez Neto <juoliveiraneto2512@gmail.com>
2020-04-10 00:28:44 -0300

    Implementação do Swagger

--- produtosApi/src/main/java/com/summit/api/controller/ProdutoController.java
+++ produtosApi/src/main/java/com/summit/api/controller/ProdutoController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import com.summit.api.datasource.model.Produto;
 import com.summit.api.exception.ProdutoResourceException;
 import com.summit.api.resource.model.ProdutoResource;
@@ -20,6 +23,7 @@ import com.summit.api.service.UpdateProduto;
 import com.summit.api.service.BuscarPorId;
 import com.summit.api.service.BuscarProdutos;
 
+@Api(value = "API REST Produtos")
 @RestController
 @RequestMapping(value = "/api")
 public class ProdutoController {
@@ -39,26 +43,31 @@ public class ProdutoController {
 	@Autowired
 	private UpdateProduto serviceUpdateProduto;
 	
+	@ApiOperation(value = "Retorna uma lista de Produtos")
 	@GetMapping(path = "/produtos")
 	public List<Produto> getAll() {
 		return serviceBuscarTodos.listaProdutos();
 	}
 	
+	@ApiOperation(value = "Retorna um produto de acordo com o ID")
 	@GetMapping(path = "/produto/id/{id}")
 	public Produto getById(@PathVariable (name =  "id", required = true) Long id) throws Exception {
 		return serviceBuscarPorId.buscarPorId(id);
 	}
 	
+	@ApiOperation(value = "Cadastra um produto")
 	@PostMapping(path = "/produto/save")
 	public void saveProduto(@RequestBody ProdutoResource produto) throws ProdutoResourceException {
 		serviceCadastro.cadastro(produto);
 	}
 	
+	@ApiOperation(value = "Deleta um produto pelo ID")
 	@DeleteMapping(path = "/produto/delete/{id}")
 	public void deleteProduto(@PathVariable (name = "id", required = true) Long id) throws Exception {
 		serviceDeletarPorId.deletarPorId(id);
 	}
 	
+	@ApiOperation(value = "Atualiza os dados de um produto pelo ID")
 	@PutMapping(path = "/produto/update/{id}")
 	public void updateProduto(@PathVariable (name = "id", required = true) Long id, 
 			@RequestBody ProdutoResource produto) throws Exception {
